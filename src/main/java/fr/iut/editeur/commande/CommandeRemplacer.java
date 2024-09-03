a6941967d94dede0a96e97fda2eae6419afeabbb
virgil <virgil.dritzas@etu.umontpellier.fr>
2024-09-03 17:12:13 +0200

    correction d'erreur dans la classe CommandeRemplacer

--- src/main/java/fr/iut/editeur/commande/CommandeRemplacer.java
+++ src/main/java/fr/iut/editeur/commande/CommandeRemplacer.java
@@ -10,14 +10,16 @@ public class CommandeRemplacer extends CommandeDocument{
 
     @Override
     public void executer() {
-        if(parameters.length < 4) {
+        if(parameters.length < 3) {
             System.err.println("Format attendu : remplacer;debut;fin;chaine");
             return;
         }
         Integer debut = Integer.parseInt(parameters[1]);
         Integer fin = Integer.parseInt(parameters[2]);
         String chaine = "";
-        chaine += parameters[3];
+        if(parameters.length == 4) {
+            chaine = parameters[3];
+        }
         this.document.remplacer(debut,fin,chaine);
         super.executer();
     }
