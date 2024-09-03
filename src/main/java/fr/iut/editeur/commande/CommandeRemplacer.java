892987dc56660365a197ed13c7167d1714d260b6
virgil <virgil.dritzas@etu.umontpellier.fr>
2024-09-03 16:49:22 +0200

    ajout méthode remplacer
    nouvelle classe CommandeRemplacer
    
    méthode complète executer de CommandeRemplacer
    
    erreur corrigé de executer de CommandeRemplacer

---
+++ src/main/java/fr/iut/editeur/commande/CommandeRemplacer.java
@@ -0,0 +1,24 @@
+package fr.iut.editeur.commande;
+
+import fr.iut.editeur.document.Document;
+
+public class CommandeRemplacer extends CommandeDocument{
+
+    public CommandeRemplacer(Document document, String[] parameters) {
+        super(document, parameters);
+    }
+
+    @Override
+    public void executer() {
+        if(parameters.length < 4) {
+            System.err.println("Format attendu : remplacer;debut;fin;chaine");
+            return;
+        }
+        Integer debut = Integer.parseInt(parameters[1]);
+        Integer fin = Integer.parseInt(parameters[2]);
+        String chaine = "";
+        chaine += parameters[3];
+        this.document.remplacer(debut,fin,chaine);
+        super.executer();
+    }
+}
